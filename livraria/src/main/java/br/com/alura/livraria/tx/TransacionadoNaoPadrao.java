6159f15518d7541196227226abf8c611e4ff11ab
José Geraldo Costa Oliveira <e539740@mail.caixa>
2019-04-01 15:49:45 -0300

    mudanças relacionadas a aula 4 do curso de cdi

--- livraria/src/main/java/br/com/alura/livraria/tx/TransacionadoNaoPadrao.java
+++ livraria/src/main/java/br/com/alura/livraria/tx/TransacionadoNaoPadrao.java
@@ -1,22 +1,41 @@
 package br.com.alura.livraria.tx;
 
-import javax.enterprise.inject.Specializes;
+import javax.annotation.Priority;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Typed;
+import javax.inject.Inject;
+import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
 
-import br.com.ultcode.lib.tx.TransacionadoPadrao;
+import br.com.ultcode.lib.tx.Transacionado;
 
-@Specializes
-public class TransacionadoNaoPadrao extends TransacionadoPadrao {
+@Alternative
+@Priority(Interceptor.Priority.APPLICATION)
+@Typed(Transacionado.class)
+public class TransacionadoNaoPadrao implements Transacionado {
 
 	private static final long serialVersionUID = 5554481286148016045L;
+
+	@Inject
+	EntityManager manager;
 
 	@Override
 	public Object gerenciaTransacao(InvocationContext context) {
 
-		System.out.println("Faz algo antes");
-		Object gerenciaTransacao = super.gerenciaTransacao(context);
-		System.out.println("Faz algo depois");
-		return gerenciaTransacao;
+		System.out.println("Antes de abrir a transação");
+		manager.getTransaction().begin();
+		try {
+			Object proceed = context.proceed();
+			System.out.println("Antes de fechar a transação");
+			manager.getTransaction().commit();
+
+			return proceed;
+		} catch (Exception e) {
+			System.out.println("Antes de dar rollback");
+			manager.getTransaction().rollback();
+			throw new RuntimeException(e);
+		}
 	}
-	
+
 }
